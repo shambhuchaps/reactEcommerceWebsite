379e63df5065fd08e34675200a803a9eaabcb381
shambhu chapagain <102132803+shambhuchaps@users.noreply.github.com>
2023-02-17 12:47:00 +0545

    Complete react-routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Cart from "./components/Cart";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import Home from "./components/Home";
+import Products from "./components/Products";
+import SingleProduct from "./components/SingleProduct";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/products" element={<Products/>} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/singleproduct/:id" element={<SingleProduct />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="*" element={<Error />} />
+     </Routes>
+    </BrowserRouter>
   );
 }
 
