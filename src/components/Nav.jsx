8dabc57ae8763bf71e9a835ff2ff21d31d6f9e27
shambhu chapagain <102132803+shambhuchaps@users.noreply.github.com>
2023-02-22 09:13:39 +0545

    responsive navbar complete

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { FiShoppingCart } from "react-icons/fi";
+import { CgMenu, CgClose } from 'react-icons/cg';
 
 const Nav = () => {
+  const [menuIcon, setMenuIcon] = useState();
 
   const Nav = styled.nav`
   .navbar-lists {
@@ -49,23 +51,99 @@ const Nav = () => {
       background-color: ${({ theme }) => theme.colors.helper};
     }
   }
+
+  .mobile-navbar-btn {
+    display: none;
+    background-color: transparent;
+    cursor: pointer;
+    border: none;
+  }
+  .mobile-nav-icon[name="close-outline"] {
+    display: none;
+  }
+  .close-outline {
+    display: none;
+  }
+
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .mobile-navbar-btn {
+      display: inline-block;
+      z-index: 9999;
+      border: ${({ theme }) => theme.colors.black};
+      .mobile-nav-icon {
+        font-size: 4.2rem;
+        color: ${({ theme }) => theme.colors.black};
+      }
+    }
+    .active .mobile-nav-icon {
+      display: none;
+      font-size: 4.2rem;
+      position: absolute;
+      top: 30%;
+      right: 10%;
+      color: ${({ theme }) => theme.colors.black};
+      z-index: 9999;
+    }
+    .active .close-outline {
+      display: inline-block;
+    }
+    .navbar-lists {
+      width: 100vw;
+      height: 100vh;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-color: #fff;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      visibility: hidden;
+      opacity: 0;
+      transform: translateX(100%);
+      /* transform-origin: top; */
+      transition: all 3s linear;
+    }
+    .active .navbar-lists {
+      visibility: visible;
+      opacity: 1;
+      transform: translateX(0);
+      z-index: 999;
+      transform-origin: right;
+      transition: all 3s linear;
+      .navbar-link {
+        font-size: 4.2rem;
+      }
+    }
+    .cart-trolley--link {
+      position: relative;
+      .cart-trolley {
+        position: relative;
+        font-size: 5.2rem;
+      }
+      .cart-total--item {
+        width: 4.2rem;
+        height: 4.2rem;
+        font-size: 2rem;
+      }
+    }
 `;
 
   return (
     <Nav>
-      <div className="navbar">
-        <div className="navbar-lists">
+      <div className={menuIcon ? "navbar active" : "navbar"}>
+        <ul className="navbar-lists">
           <li>
-            <NavLink to="/" className='navbar-link home-link'>Home</NavLink>
+            <NavLink to="/" className='navbar-link home-link' onClick={() => setMenuIcon(false)}>Home</NavLink>
           </li>
           <li>
-            <NavLink to="/about" className='navbar-link '>About</NavLink>
+            <NavLink to="/about" className='navbar-link ' onClick={() => setMenuIcon(false)}>About</NavLink>
           </li>
           <li>
-            <NavLink to="/products" className='navbar-link '>Products</NavLink>
+            <NavLink to="/products" className='navbar-link ' onClick={() => setMenuIcon(false)}>Products</NavLink>
           </li>
           <li>
-            <NavLink to="/contact" className='navbar-link '>Contact</NavLink>
+            <NavLink to="/contact" className='navbar-link ' onClick={() => setMenuIcon(false)}>Contact</NavLink>
           </li>
           <li>
             <NavLink to='/cart' className='navbar-link cart-trolley--link'>
@@ -73,6 +151,12 @@ const Nav = () => {
               <span className='cart-total--item'>10</span>
             </NavLink>
           </li>
+        </ul>
+
+        {/* two button for open and close of menu  */}
+        <div className="mobile-navbar-btn">
+          <CgMenu name='menu-outline' className="mobile-nav-icon" onClick={() => setMenuIcon(true)} />
+          <CgClose name='close-outline' className="mobile-nav-icon close-outline" onClick={() => setMenuIcon(false)} />
         </div>
       </div>
     </Nav>
