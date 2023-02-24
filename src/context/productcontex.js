c59e7b881dde3e24d8f66fe84720d356e81ad3e7
shambhu chapagain <102132803+shambhuchaps@users.noreply.github.com>
2023-02-24 12:46:31 +0545

    Fetch API data using axios

--- src/context/productcontex.js
+++ src/context/productcontex.js
@@ -2,12 +2,27 @@
 // provider
 // consumer => useContext Hook
 
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
 
 
 const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
 const AppProvider = ({children}) => {
+
+const getProducts = async (url) => {
+  const res = await axios.get(url);
+  // console.log(res);
+  const products = await res.data;
+  console.log(products);
+};
+
+  useEffect(() => {
+    getProducts(API);
+  }, [])
+  
   return <AppContext.Provider value={{myName:"shambhu chapagain"}}>{children}</AppContext.Provider>
 };
 
