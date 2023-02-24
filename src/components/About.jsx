3e542dd066b75768f28cbd28c64590cf62a69823
shambhu chapagain <102132803+shambhuchaps@users.noreply.github.com>
2023-02-24 12:15:48 +0545

    concept of usecontext and custom hooks

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
+// import React, {useContext} from 'react'
+// import { AppContext} from '../context/productcontex'
+import { useProductContext } from '../context/productcontex'
 import HeroSection from './HeroSection'
 
 const About = () => {
+  // const {myName} = useContext(AppContext);
+  
+    // another way to use
+  const {myName} = useProductContext();  
 
   const data = {
     name: "Thapa Ecommerce",
   }
 
   return (
+    <>
+     <p>{myName}</p>
     <HeroSection myData={data}/>
+    </>
   )
 }
 
