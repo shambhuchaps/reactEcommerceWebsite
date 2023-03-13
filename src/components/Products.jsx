f7ded7a7536c1b035bd57148f7122fca3df37731
shambhu chapagain <102132803+shambhuchaps@users.noreply.github.com>
2023-03-13 21:33:36 +0545

    Get data for products page using context api & useReducer Hook

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import { useFilterContext } from "../context/filter_context";
+import FilterSection from "./FilterSection";
+import ProductList from "./ProductList";
+import Sort from "./Sort";
 
 const Products = () => {
-  return (
-    <div>Products</div>
-  )
-}
 
-export default Products
+   const {filter_products} = useFilterContext();
+   console.log(filter_products);
+
+  return <Wrapper>
+    <div className="container grid grid-filter-column">
+      <div>
+        <FilterSection />
+      </div>
+
+      <section className="product-view--sort">
+        <div className="sort-filter">
+          <Sort />
+        </div>
+        <div className="main-product">
+          <ProductList />
+        </div>
+      </section>
+    </div>
+  </Wrapper>;
+};
+
+const Wrapper = styled.section`
+  .grid-filter-column {
+    grid-template-columns: 0.2fr 1fr;
+  }
+  @media (max-width: ${({ theme }) => theme.media.mobile}) {
+    .grid-filter-column {
+      grid-template-columns: 1fr;
+    }
+  }
+`;
+
+export default Products;
