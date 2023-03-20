f1b17c1f267397c7dd613a9e482a3900519acc9a
shambhu chapagain <102132803+shambhuchaps@users.noreply.github.com>
2023-03-20 23:18:50 +0545

    easy way to sorting products

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -28,15 +28,16 @@ export const FilterContextProvider = ({children}) => {
     return dispatch({type:"SET_LISTVIEW"})
   }
 
-  const sorting = ()=>{
-    return dispatch({type: "GET_SORT_VALUE"})
+  const sorting = (event)=>{
+    let userValue = event.target.value;
+    return dispatch({type: "GET_SORT_VALUE", payload: userValue})
   };
 
   // to sort the product
   useEffect(() =>{
     //  console.log("hello");
-    dispatch({type: "SORTING_PRODUCTS", payload: products});
-  }, [state.sorting_value]);
+    dispatch({type: "SORTING_PRODUCTS"});
+  }, [products, state.sorting_value]);
 
   useEffect(() =>{
      dispatch({type:"LOAD_FILTER_PRODUCTS", payload: products });
