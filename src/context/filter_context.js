bc446adb2df2014a0746dd1800f715ff7ba138c7
shambhu chapagain <102132803+shambhuchaps@users.noreply.github.com>
2023-03-25 15:02:05 +0545

    complete search filter section

--- src/context/filter_context.js
+++ src/context/filter_context.js
@@ -10,6 +10,9 @@ const initialState = {
   all_products:[],
   grid_view: true,
   sorting_value:"lowest",
+  filters:{
+    text: "",
+  }
 };
 
 export const FilterContextProvider = ({children}) => {
@@ -28,23 +31,33 @@ export const FilterContextProvider = ({children}) => {
     return dispatch({type:"SET_LISTVIEW"})
   }
 
+  //sorting function
   const sorting = (event)=>{
     let userValue = event.target.value;
     return dispatch({type: "GET_SORT_VALUE", payload: userValue})
   };
 
+  // update the filter values
+  const updateFilterValue = (event) =>{
+      let name = event.target.name;
+      let value = event.target.value;
+
+      return dispatch({type:"UPDATE_FILTERS_VALUE", payload:{name, value}});
+  }
+
   // to sort the product
   useEffect(() =>{
     //  console.log("hello");
+    dispatch({type:"FILTER_PRODUCTS"})
     dispatch({type: "SORTING_PRODUCTS"});
-  }, [products, state.sorting_value]);
+  }, [products, state.sorting_value, state.filters]);
 
   useEffect(() =>{
      dispatch({type:"LOAD_FILTER_PRODUCTS", payload: products });
   }, [products]);
 
    return (
-   <FilterContext.Provider value={{ ...state, setGridView, setListView, sorting }}>
+   <FilterContext.Provider value={{ ...state, setGridView, setListView, sorting, updateFilterValue }}>
     {children}
    </FilterContext.Provider>
    );
