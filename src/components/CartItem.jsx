c3a8ff1426a7b5c9424c973ae4b603043121b8f4
shambhu chapagain <102132803+shambhuchaps@users.noreply.github.com>
2023-03-29 16:40:55 +0545

    complete cart and cartItem page

---
+++ src/components/CartItem.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { FaTrash } from 'react-icons/fa';
+import FormatPrice from "../Helpers/FormatePrice";
+import CartAmountToggle from './CartAmountToggle';
+
+const CartItem = ({id, name, image, color, price, amount}) => {
+
+  // const [amount, setAmount] = useState(1);
+
+  const setDecrease = () => {
+    // amount > 1 ? setAmount(amount - 1) : setAmount(1);
+  };
+
+  const setIncrease = () => {
+    // amount < stock ? setAmount(amount + 1) : setAmount(stock);
+  };
+
+  return (
+    <div className="cart_heading grid grid-five-column">
+      <div className="cart-image--name">
+        <div>
+          <figure>
+            <img src={image} alt={id} />
+          </figure>
+        </div>
+        <div>
+          <p>{name}</p>
+          <div className="color-div">
+            <p>color:</p>
+            <div className='color-style' style={{backgroundColor: color, color:color}}></div>
+          </div>
+        </div>
+      </div>
+
+      <div className="cart-hide">
+        <p><FormatPrice price={price} /></p>
+      </div>
+
+      <CartAmountToggle amount={amount} setDecrease={setDecrease} setIncrease={setIncrease} />
+
+      <div className="cart-hide">
+        <p><FormatPrice price={price*amount}/></p>
+      </div>
+
+      <div>
+        <FaTrash className='remove_icon' />
+      </div>
+    </div>
+  )
+}
+
+export default CartItem
--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -3,7 +3,24 @@ const cartReducer = (state, action) => {
 
   if (action.type === "ADD_TO_CART") {
     let { id, color, amount, product } = action.payload;
-    console.log(product);
+    // console.log(product);
+
+   let cartProduct;
+
+   cartProduct = {
+    id: id + color,
+    name: product.name,
+    color,
+    amount,
+    image: product.image[0].url,
+    price: product.price,
+    max: product.stock,
+   }
+
+    return {
+      ...state,
+      cart : [...state.cart, cartProduct],
+    }
   }
 
   return state;
